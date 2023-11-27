5c6a5d4873aea4ce35b9fd11c69e226dfed4be04
Peter Carlyle <peter.carlyle@bravecmedia.co.uk>
2023-11-27 18:04:39 +0000

    Projects Initial page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -83,7 +83,7 @@ export default async function Projects(props: PageProps) {
                   </Card.Link>
                 </h2>
                 <Card.Description>
-                  {item.data.shortDescription}
+                  {item?.data?.shortDescription}
                 </Card.Description>
                 <p className='relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200'>
                   <LinkIcon className='h-6 w-6 flex-none' />
--- components/ProjectTechnologies/index.tsx
+++ components/ProjectTechnologies/index.tsx
@@ -4,7 +4,7 @@ export default function ProjectTechnologies(props: any) {
       role='list'
       className='mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6'
     >
-      {props.technology.map((tech) => (
+      {props.technology.map((tech: any) => (
         <li key={tech.name}>
           <img
             className='mx-auto h-24 w-24 rounded-full'
