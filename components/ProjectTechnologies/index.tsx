4b19a28bf24a94a7e7cafdfab9b0e4adfea7a8a2
Peter Carlyle <peter.carlyle@bravecmedia.co.uk>
2023-11-28 11:11:01 +0000

    fix Image sizes

--- components/ProjectTechnologies/index.tsx
+++ components/ProjectTechnologies/index.tsx
@@ -2,7 +2,7 @@ export default function ProjectTechnologies(props: any) {
   return (
     <>
       <div className='flex flex-row'>
-        <h4 className='title'>Tech Integrations</h4>{' '}
+        <h4 className='title'>Tech Used</h4>{' '}
       </div>
       <div className='flex flex-row'>
         <div className='flex flex-row flex-wrap'></div>
@@ -11,6 +11,7 @@ export default function ProjectTechnologies(props: any) {
             <>
               <div className='grid p-5' key={tool?.tech?.value?.data?.name}>
                 <img
+                  className={'h-20 w-20'}
                   src={tool?.tech?.value?.data?.logo}
                   alt={tool?.tech?.value?.data?.name}
                   aria-description={tool?.tech?.value?.data?.name}
